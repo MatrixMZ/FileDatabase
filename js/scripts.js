4e4c45b198455db64b799c10c068704f58cbcb2d
Mateusz Ziobrowski <matrix.ziobrowski@gmail.com>
2017-01-08 20:36:48 +0100

    Secound event working

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,19 @@ $("span.glyphicon-pencil").click(function(){
     var one = $('tr[row='+row+'] td[num=1]').text();
     var two = $('tr[row='+row+'] td[num=2]').text();
     var three = $('tr[row='+row+'] td[num=3]').text();
-    $('tr[row='+row+']').html('<td>'+row+'</td><td><input value="'+one+'" id="name'+row+'" type="text" class="form-control" /></td><td><input id="lastname'+row+'" value="'+two+'" type="text" class="form-control" /></td><td><input id="pesel'+row+'" value="'+three+'" type="text" class="form-control" /></td><td><span class="sendedit glyphicon glyphicon-ok"></span> <span class="glyphicon glyphicon-remove"></span></td>');
+    $('tr[row='+row+']').html('<td>'+row+'</td><td><input value="'+one+'" id="name'+row+'" type="text" class="form-control" /></td><td><input id="lastname'+row+'" value="'+two+'" type="text" class="form-control" /></td><td><input id="pesel'+row+'" value="'+three+'" type="text" class="form-control" /></td><td><span class="sendedit glyphicon glyphicon-ok"></span> <span class="glyphicon glyphicon-remove reloadpage"></span></td>');
 });
-$('.sendedit').click(function(){
-    var row = $(this).closest('tr').attr('row');
-    //var name = $('input[id=name'+row+']').attr('value');
-   alert('kappa');
-    $(this).html(row);
+$(document).ready(function(){
+    $(document).on("click", ".sendedit", function(){
+        var row = $(this).closest('tr').attr('row');
+        //var name = $('input[id=name'+row+']').attr('value');
+        alert('kappa');
+        $(this).html(row);
+    });
+    $(document).on("click", ".reloadpage", function(){
+        alert('reload');
+        location.reload();
+    });
 });
 $("#adduser").click(function(){
     var pesel = $('#pesel').val();
